cbf65118f63f6b35d9e0d9fb3fe826a3e74d6120
FourLeafClover <zc_smile@outlook.com>
2014-10-28 21:23:22 -0700

    C# Attribute And Reflect

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReflectTest.Model;
+using ReflectTest.Business;
+
+namespace ReflectTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person p1 = new Person() { Address="chengdu,tianfuruanjianyuan" };
+
+            Console.WriteLine(PersonCheck.GetErrorMessage(p1));
+
+            Console.ReadKey();
+            
+        }
+    }
+}
